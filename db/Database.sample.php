d4bd87265b5bacf25cf759a8c67688d92a3f1e92
Mahdi Amirikhamkani <mehdiamiri321@gmail.com>
2025-08-07 15:49:23 +0200

    Add sample database  file

---
+++ db/Database.sample.php
@@ -0,0 +1,33 @@
+<?php
+
+// Stellt eine Verbindung zur MySQL-Datenbank her
+class Database {
+    private $conn;
+
+    // Verbindung im Konstruktor aufbauen
+    public function __construct() {
+        $servername = '*****'; 
+        $username = '*****';
+        $password = '*****';
+        $dbname = '*****';
+
+        $this->conn = new mysqli($servername, $username, $password, $dbname);
+    
+        // Fehler beim Verbindungsaufbau abfangen
+        if ($this->conn->connect_error) {
+            die(json_encode([        
+                'status' => 'error',
+                'message' => 'DB connection failed: ' . $this->conn->connect_error
+            ]));
+        }
+    }
+
+    // Zugriff auf die aktive Verbindung ermöglichen
+    public function getConnection() {
+        return $this->conn;
+    }
+}
+
+?>
+
+
